ac0c7f35e765d14a5bc2e5c98e50c80df98a6e0e
Jason Barnett <jason.w.barnett@gmail.com>
2018-01-05 18:03:02 -0500

    updates

--- cmd/ktremove.go
+++ cmd/ktremove.go
@@ -7,7 +7,8 @@ import (
 )
 
 var principal string
-var kvno, all, old bool
+var kvno int
+var all, old bool
 
 // ktremoveCmd represents the ktremove command
 var removeCmd = &cobra.Command{
@@ -15,12 +16,15 @@ var removeCmd = &cobra.Command{
 	Short: "Remove a Service Principal Name (SPN) from a Keytab file",
 	Long: `Remove a Service Principal Name (SPN) from a Keytab file
 
-gokutil remove --keytab /etc/krb5.keytab --
-and usage of using your command. For example:
+Remove all associated keys:
+  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --all
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Remove old associated keys:
+  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --old
+
+Remove kvno associated keys:
+  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --kvno 2
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		MyCmd()
 		// fmt.Println("Jason")
@@ -35,7 +39,7 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(removeCmd)
 	removeCmd.Flags().StringVar(&principal, "principal", "", "Specifies the principal to be removed from the keytab file.")
-	removeCmd.Flags().BoolVar(&kvno, "kvno", false, "Removes all entries for the specified principal whose key version number matches kvno.")
+	removeCmd.Flags().IntVar(&kvno, "kvno", 0, "Removes all entries for the specified principal whose key version number matches kvno.")
 	removeCmd.Flags().BoolVar(&all, "all", false, "Removes all entries for the specified principal.")
 	removeCmd.Flags().BoolVar(&old, "old", false, "Removes all entries for the specified principal, except those principals with the highest key version number.")
 
