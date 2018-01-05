4226f9e245ee954cbf8266aef5df4bdec5744946
Jason Barnett <jason.w.barnett@gmail.com>
2018-01-05 18:03:35 -0500

    remove comments

--- cmd/ktremove.go
+++ cmd/ktremove.go
@@ -42,16 +42,6 @@ func init() {
 	removeCmd.Flags().IntVar(&kvno, "kvno", 0, "Removes all entries for the specified principal whose key version number matches kvno.")
 	removeCmd.Flags().BoolVar(&all, "all", false, "Removes all entries for the specified principal.")
 	removeCmd.Flags().BoolVar(&old, "old", false, "Removes all entries for the specified principal, except those principals with the highest key version number.")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ktremoveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ktremoveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func MyCmd() {
