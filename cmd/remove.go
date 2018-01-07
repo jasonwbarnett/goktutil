baa713facc346cef791505105eea545bc448c56a
Jason Barnett <jason.w.barnett@gmail.com>
2018-01-06 19:23:01 -0500

    finish flags

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -17,14 +18,30 @@ var removeCmd = &cobra.Command{
 	Long: `Remove a Service Principal Name (SPN) from a Keytab file
 
 Remove all associated keys:
-  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --all
+  gokutil remove --keytab /etc/krb5.keytab HTTP/MYHOST --all
 
 Remove old associated keys:
-  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --old
+  gokutil remove --keytab /etc/krb5.keytab HTTP/MYHOST --old
 
 Remove kvno associated keys:
-  gokutil remove --keytab /etc/krb5.keytab --principal HTTP/MYHOST --kvno 2
+  gokutil remove --keytab /etc/krb5.keytab HTTP/MYHOST --kvno 2
 `,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if kvno == 0 && all == false && old == false {
+			return errors.New("Must at least one of: --kvno, --all or --all")
+		}
+		if kvno != 0 && all != false {
+			return errors.New("Only pass one of: --kvno or --all")
+		}
+		if kvno != 0 && old != false {
+			return errors.New("Only pass one of: --kvno or --old")
+		}
+		if all != false && old != false {
+			return errors.New("Only pass one of: --all or --old")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		MyCmd()
 		// fmt.Println("Jason")
@@ -39,6 +56,7 @@ Remove kvno associated keys:
 func init() {
 	rootCmd.AddCommand(removeCmd)
 	removeCmd.Flags().StringVar(&principal, "principal", "", "Specifies the principal to be removed from the keytab file.")
+	removeCmd.MarkFlagRequired("principal")
 	removeCmd.Flags().IntVar(&kvno, "kvno", 0, "Removes all entries for the specified principal whose key version number matches kvno.")
 	removeCmd.Flags().BoolVar(&all, "all", false, "Removes all entries for the specified principal.")
 	removeCmd.Flags().BoolVar(&old, "old", false, "Removes all entries for the specified principal, except those principals with the highest key version number.")
